19c891855dc594349cdac05f371133a48569235e
hitmanet <nathan200033@gmail.com>
2020-05-14 16:14:57 +0300

    feature(configs): добавление конфигов

---
+++ webpack.config.js
@@ -0,0 +1,140 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const styleLoader = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+module.exports = ({ root, entry, port = 3000 }) => ({
+  mode: isProduction ? 'production' : 'development',
+
+  entry: path.resolve(root, entry),
+
+  output: {
+    publicPath: process.env.PUBLIC_PATH || './',
+  },
+
+  optimization: isProduction
+    ? {
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              output: {
+                comments: false,
+              },
+            },
+          }),
+        ],
+      }
+    : undefined,
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          styleLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              localsConvention: 'camelCase',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            // eslint-disable-next-line global-require
+            options: require('./postcss.config'),
+          },
+        ],
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.(js|ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              configFile: false,
+              cacheDirectory: true,
+              cacheCompression: isProduction,
+              compact: isProduction,
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              compiler: 'ttypescript',
+              context: root,
+            },
+          },
+        ].filter(Boolean),
+      },
+      {
+        test: /\.(woff|woff2|(o|t)tf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            query: {
+              name: `assets/fonts/[name]${isProduction ? '.[hash]' : ''}.[ext]`,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            query: {
+              name: `assets/img/[name]${isProduction ? '.[hash]' : ''}.[ext]`,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    modules: [path.resolve(root, 'node_modules')],
+    alias: {
+      '@': path.resolve(root, 'src'),
+    },
+    symlinks: false,
+  },
+
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+
+  devServer: {
+    contentBase: path.resolve(root, './public'),
+    port,
+  },
+
+  plugins: [
+    isProduction && new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({ React: 'react' }),
+    process.env.ANALYZE && new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(root, './public/index.html'),
+      inject: 'body',
+    }),
+  ].filter(Boolean),
+});
